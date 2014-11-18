7abb68c6016cd6852b3e39aca95b2041eb627401
Tobias Sarnowski <tobias.sarnowski@zalando.de>
2014-11-18 09:45:16 +0100

    added datomic dependency

--- project.clj
+++ project.clj
@@ -10,11 +10,16 @@
                  [metosin/ring-http-response "0.5.2"]
                  [metosin/ring-swagger-ui "2.0.17"]
                  [org.clojure/tools.logging "0.2.4"]
-                 ;[com.datomic/datomic-pro "0.9.5067"]
                  [amazonica "0.2.30" :exclusions [joda-time]]]
 
   :main aws-overlord.core
   :uberjar-name "aws-overlord.jar"
   :profiles {:uberjar {:resource-paths ["swagger-ui"]}
+
+             :free {:dependencies [[com.datomic/datomic-free "0.9.5067"]]}
+             :pro {:dependencies [[com.datomic/datomic-pro "0.9.5067"]]
+                   :repositories {"my.datomic.com" {:url "https://my.datomic.com/repo"
+                                                    :creds :gpg}}}
+
              :dev {:dependencies [[javax.servlet/servlet-api "2.5"]
                                   [org.slf4j/slf4j-simple "1.7.7"]]}})
